6828dd00d1e60f70504c0770dcc0a79046db8c05
Seobs <subi88@naver.com>
2023-01-29 09:47:08 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // variable to call and store future list of posts
   // Future<List> goodsFuture = getGoods();
+  List goods = [];
 
   // function to fetch data from api and return future list of posts
   getGoods() async {
     var url = Uri.parse("https://3u553ufx3k.execute-api.ap-northeast-2.amazonaws.com/dev/goods");
     final response = await http.get(url, headers: {"Content-Type": "application/json"});
-    log(response.body);
+    // log(response.body);
     // final List body = json.decode(response.body);
+
+    goods = json.decode(response.body);
+    // log(goods.toString());
 
     // return body.map((e) => Post.fromJson(e)).toList();
   }
@@ -53,8 +57,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     const title = 'Basic List';
 
-    final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100, 100, 100, 100, 100, 100, 100];
+    // final List<String> entries = <String>['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C'];
+    // final List<int> colorCodes = <int>[600, 500, 100, 100, 100, 100, 100, 100, 100];
 
     return MaterialApp(
       title: title,
@@ -64,13 +68,16 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: ListView.builder(
             padding: const EdgeInsets.all(5),
-            itemCount: entries.length,
+            itemCount: goods.length,
             itemBuilder: (BuildContext context, int index) {
+              log(goods[index].toString());
+
               return Container(
                 height: 105,
                 margin: const EdgeInsets.all(5.0),
                 padding: const EdgeInsets.all(15),
                 // color: Colors.amber[colorCodes[index]],
+                color: Colors.grey,
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
@@ -81,16 +88,16 @@ class _MyHomePageState extends State<MyHomePage> {
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
-                                const Text(
-                                  '부사',
-                                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                                Text(
+                                  goods[index]['name'],
+                                  style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                                 ),
                                 Row(children: <Widget>[
                                   // container 12개 필요한데 for를 쓸 수 없나?
                                   // - 네네 for문을 {} 안에 감싸면 안돼서
                                   // - for 밑에 딱 하나의 선언문만 들어갈 수 있어요
                                   // - 그래서 여러개 하고 싶으면 따로 메서드로 뺴서 그 메서드를 호출해야해요
-                                  for (int i = 0; i < 12; i++)
+                                  for (int i = 1; i <= 12; i++)
                                     Container(
                                       margin: const EdgeInsets.only(right: 2.0),
                                       height: 20,
@@ -98,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       decoration: BoxDecoration(
                                         shape: BoxShape.circle,
                                         border: Border.all(width: 1, color: Colors.red),
-                                        color: (i > 9) ? Colors.red : Colors.transparent,
+                                        color: (i == 1) ? Colors.red : Colors.transparent,
                                       ),
                                       child: Center(
                                         // text에 const가 붙고 안붙고는 어떤 차이가 있을까?
